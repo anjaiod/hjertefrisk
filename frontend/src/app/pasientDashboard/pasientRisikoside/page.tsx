5b4f4513417b578aec7fd5926600f2e3717e26be
agnesaven <agnesuhr@gmail.com>
2026-03-19 13:31:51 +0100

    feat: risk-page for patient with basic theme buttons
    Risk-page for patients now accessible through button from dashboard. The buttons now lead to the empty measures page, and will later need to lead to the specific measure clicked on.

--- frontend/src/app/pasientDashboard/pasientRisikoside/page.tsx
+++ frontend/src/app/pasientDashboard/pasientRisikoside/page.tsx
@@ -1,5 +1,10 @@
+"use client";
+
 import { PatientSidebarNav } from "../../../components/organisms/PatientSidebarNav";
 import { PatientHeader } from "../../../components/organisms/PatientHeader";
+import { IconButton } from "@/components/atoms/IconButton";
+import { HealthCard } from "@/components/molecules/HealthCard";
+import { SectionWrapper } from "@/components/organisms/SectionWrapper";
 
 export default function Page() {
   return (
@@ -10,7 +15,56 @@ export default function Page() {
         <PatientHeader />
 
         <main className="p-8">
-          <h1 className="text-2xl font-semibold">Din risikoprofil</h1>
+          <div className="mb-4">
+            <div className="flex items-center gap-3">
+              <IconButton
+                onClick={() =>
+                  (window.location.href = "../../pasientDashboard")
+                }
+                ariaLabel="Back to dashboard"
+              >
+                ←
+              </IconButton>
+
+              <h1 className="text-2xl font-semibold text-brand-navy">
+                Din somatiske helseoversikt
+              </h1>
+            </div>
+          </div>
+          <p className="text-sm text-brand-sage">
+            Trykk på et kort for å lese mer
+          </p>
+          <div>
+            <SectionWrapper title="Levevaner">
+              <HealthCard title="Fysisk aktivitet" />
+              <HealthCard title="Kosthold" />
+              <HealthCard title="Overvekt" />
+              <HealthCard title="Søvn" />
+            </SectionWrapper>
+
+            <SectionWrapper title="Livsstilsvaner">
+              <HealthCard title="Røyking" />
+              <HealthCard
+                title="Alkohol" // placeholder description
+                description="AUDIT skåre: 3 "
+              />
+              <HealthCard title="Rus" />
+              <HealthCard title="Tannhelse" />
+            </SectionWrapper>
+
+            <SectionWrapper title="Annet">
+              <HealthCard
+                title="Blodlipider"
+                // placeholder description
+                description="Totalkolesterol ≥ 7,0 mmol/l"
+              />
+              <HealthCard title="Glukose" />
+              <HealthCard
+                title="Blodtrykk" // placeholder description
+                description={"≥ 140 systolisk\n≥ 90 diastolisk (mmHg)"}
+              />
+            </SectionWrapper>
+          </div>
         </main>
       </div>
     </div>
