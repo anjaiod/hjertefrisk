6d1a373412a913192207ab89107d4c255c2547cf
johanneskhage <johannesk.hage@gmail.com>
2026-04-21 12:13:17 +0200

    revert earlier commit to make pipeline pass

--- backend/Migrations/20260421082737_AddToDoCreatedAtAndRuleId.cs
+++ backend/Migrations/20260421082737_AddToDoCreatedAtAndRuleId.cs
@@ -6,7 +6,7 @@ using Microsoft.EntityFrameworkCore.Migrations;
 namespace api.Migrations
 {
     /// <inheritdoc />
-    public partial class AddToDoCreatedAt : Migration
+    public partial class AddToDoCreatedAtAndRuleId : Migration
     {
         /// <inheritdoc />
         protected override void Up(MigrationBuilder migrationBuilder)
